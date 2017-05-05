af48e1ab68d0d423ec3484a8ff6ea5ed4df53740
Ugly God <uglygod>
2017-05-05 14:33:36 -0700

    Initializing world

---
+++ src/GameLauncher.java
@@ -0,0 +1,12 @@
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
+
+public class GameLauncher {
+	public static void main(String[] args){
+		JFrame myFrame = new JFrame();
+		myFrame.setSize(1200, 800);
+		myFrame.setVisible(true);
+	}
+}
