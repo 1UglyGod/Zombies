585278a88f9bdab7b65b731213de597708271c0b
Ugly God <uglygod>
2017-05-05 15:01:36 -0700

    Panel init

--- src/GameLauncher.java
+++ src/GameLauncher.java
@@ -6,7 +6,9 @@ import javax.swing.JFrame;
 public class GameLauncher {
 	public static void main(String[] args){
 		JFrame myFrame = new JFrame();
+		ZombiePanel myPanel = new ZombiePanel();
 		myFrame.setSize(1200, 800);
+		myFrame.add(myPanel);
 		myFrame.setVisible(true);
 	}
 }
---
+++ src/ZombiePanel.java
@@ -0,0 +1,9 @@
+import java.awt.Color;
+
+import javax.swing.JPanel;
+public class ZombiePanel extends JPanel {
+	public ZombiePanel(){
+		this.setSize(1200, 800);
+		this.setBackground(Color.black);
+	}
+}
